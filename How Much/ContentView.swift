258b0905a18dda0ed67add83d28c43967b18ec96
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-05-19 16:49:43 +0200

    - Create the keyboard

--- How Much/ContentView.swift
+++ How Much/ContentView.swift
@@ -9,9 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        numberButton(number: "1")
+        VStack {
+            HStack {
+                ForEach(7...9, id: \.self) { number in
+                    numberButton(number: String(number))
+                }
+            }
+            HStack {
+                ForEach(4...6, id: \.self) { number in
+                    numberButton(number: String(number))
+                }
+            }
+            HStack {
+                ForEach(1...3, id: \.self) { number in
+                    numberButton(number: String(number))
+                }
+            }
+            HStack {
+                numberButton(number: "0")
+                numberButton(number: ".")
+                Button {
+                    
+                } label: {
+                    Image(systemName: "delete.backward.fill")
+                        .font(.largeTitle)
+                        .bold()
+                        .frame(width: 80, height: 80)
+                        .background(.gray)
+                        .foregroundColor(.white)
+                        .clipShape(Circle())
+                }
+
+            }
+        }
         
-    
+        
     }
     
     func numberButton(number: String) -> some View {
