34f84b776302976090d53a5ed73271751d6ceb4a
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-05-19 16:21:02 +0200

    Initial Commit

---
+++ How Much/How_MuchApp.swift
@@ -0,0 +1,17 @@
+//
+//  How_MuchApp.swift
+//  How Much
+//
+//  Created by Eric on 19/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct How_MuchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
