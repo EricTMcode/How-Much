dc7349aef68f6dcd8b83ba41b0541d8a86b17b8a
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-05-20 15:21:38 +0200

    - Add a local currency converter

--- How Much/TotalView.swift
+++ How Much/TotalView.swift
@@ -11,14 +11,52 @@ struct TotalView: View {
     let total: Double
     let tipPct: Int
     let numPeople: Int
+    var tipAmount: Double {
+        total * Double(tipPct)/100
+    }
+    var totalAmount: Double {
+        total + tipAmount
+    }
+    var portion: Double {
+        totalAmount / Double(numPeople)
+    }
+    
+    let identifier = Locale.current.currency?.identifier ?? ""
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                Grid(horizontalSpacing: 20) {
+                    GridRow {
+                        Text("Original Bill")
+                            .gridColumnAlignment(.leading)
+                        Text("\(total, format: .currency(code: identifier))")
+                            .gridColumnAlignment(.trailing)
+                    }
+                    GridRow {
+                        Text("Tip")
+                        Text("\(tipAmount, format: .currency(code: identifier))")
+                    }
+                    GridRow {
+                        Text("Total")
+                        Text("\(totalAmount, format: .currency(code: identifier))")
+                    }
+                    GridRow {
+                        Text("Portion")
+                        Text("\(portion, format: .currency(code: identifier))")
+                    }
+                }
+                .font(.title)
+                Spacer()
+            }
+            .navigationTitle("Amount Owing")
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
 struct TotalView_Previews: PreviewProvider {
     static var previews: some View {
-        TotalView()
+        TotalView(total: 135.20, tipPct: 18, numPeople: 3)
     }
 }
